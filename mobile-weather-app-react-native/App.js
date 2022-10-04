9431d574caf4b102c700b183b5bbe732971b529b
Your name here <brkmer@gmail.com>
2022-10-04 12:54:21 -0400

    DateNTime component added

--- mobile-weather-app-react-native/App.js
+++ mobile-weather-app-react-native/App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-
+import DateNTime from './components/DateNTime';
 
 const img1 = require('./assets/Nice-Weather.jpg');
 export default function App() {
   return (
     <View style={styles.container}>
-    <ImageBackground source={img1} style={styles.image}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+      <ImageBackground source={img1} style={styles.image}>
+      <DateNTime />
       </ImageBackground>
     </View>
   );
