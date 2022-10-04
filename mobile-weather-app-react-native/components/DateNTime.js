88285a437cca97f3f7f99b0f6a89118c4fa0d2c3
Your name here <brkmer@gmail.com>
2022-10-04 13:14:21 -0400

    Div=View, View containers created

--- mobile-weather-app-react-native/components/DateNTime.js
+++ mobile-weather-app-react-native/components/DateNTime.js
@@ -4,7 +4,21 @@ import { View, Text } from 'react-native'
 const DateNTime = () => {
   return (
     <View>
-      <Text>Hello</Text>
+      <View>
+         <View>
+            <Text> time here</Text>
+         </View>
+         <View>
+            <Text> place here</Text>
+         </View>
+         <View>
+
+         </View>
+      </View>
+      <View>
+        <Text>location</Text>
+        <Text>lat/longitude</Text>
+      </View>
     </View>
   )
 }
