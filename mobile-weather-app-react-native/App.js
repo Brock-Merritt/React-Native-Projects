a3d7ed47cfbf3c83be7d7381f5024fc92a1b52d3
Your name here <brkmer@gmail.com>
2022-10-03 21:14:11 -0400

    same css as first weather project added, resize:cover

--- mobile-weather-app-react-native/App.js
+++ mobile-weather-app-react-native/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
+
+const img1 = require('./assets/Nice-Weather.jpg');
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ImageBackground source={img1} style={styles.image}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  image: { flex:1,
+  resize:"cover",
+  justifyContent:"center"
+}
 });
