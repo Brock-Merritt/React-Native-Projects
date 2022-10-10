ffe1f3bd8adec600f4a67a5bfe1736b9e408506d
Your name here <brkmer@gmail.com>
2022-10-10 12:32:53 -0400

    added SV IMG TXT STSH, added const styles, added main view div for scroll

--- mobile-weather-app-react-native/components/WeatherScroll.js
+++ mobile-weather-app-react-native/components/WeatherScroll.js
@@ -1,13 +1,37 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, ScrollView, Image, Text, StyleSheet } from 'react-native'
 
 const WeatherScroll = () => {
   return (
-    <view>
+    <ScrollView>
+        <CurrentTempEl></CurrentTempEl>
 
-        
-    </view>
+    </ScrollView>
   )
 }
 
+const CurrentTempEl= () => {
+
+    const img = {uri: 'http://openweathermap.org/img/wn/10d@2x.png'}
+    return (
+        <View>
+            <Image source={img} style={styles.image}/>
+            <View>
+                <Text>Sunday</Text>
+                <Text> Night - 78&#176;F</Text>
+                <Text> Day - 85&#176;F</Text>
+
+
+            </View>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    image: {
+        width: 150,
+        height: 150,
+    }
+})
 export default WeatherScroll
