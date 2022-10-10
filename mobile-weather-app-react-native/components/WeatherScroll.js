9596da8a94123aeae47d9d6d0ea13ab75ba3d9b1
Your name here <brkmer@gmail.com>
2022-10-10 12:44:18 -0400

    every text/view in cte given own stylesheet, CTC and day css added

--- mobile-weather-app-react-native/components/WeatherScroll.js
+++ mobile-weather-app-react-native/components/WeatherScroll.js
@@ -3,7 +3,7 @@ import { View, ScrollView, Image, Text, StyleSheet } from 'react-native'
 
 const WeatherScroll = () => {
   return (
-    <ScrollView>
+    <ScrollView style={styles.ScrollView}>
         <CurrentTempEl></CurrentTempEl>
 
     </ScrollView>
@@ -14,12 +14,12 @@ const CurrentTempEl= () => {
 
     const img = {uri: 'http://openweathermap.org/img/wn/10d@2x.png'}
     return (
-        <View>
+        <View style={styles.CurrentTempContainer}>
             <Image source={img} style={styles.image}/>
-            <View>
-                <Text>Sunday</Text>
-                <Text> Night - 78&#176;F</Text>
-                <Text> Day - 85&#176;F</Text>
+            <View style={styles.ForecastContainer}>
+                <Text style={styles.day}>Sunday</Text>
+                <Text style={styles.temp}> Night - 78&#176;F</Text>
+                <Text style={styles.temp}> Day - 85&#176;F</Text>
 
 
             </View>
@@ -32,6 +32,32 @@ const styles = StyleSheet.create({
     image: {
         width: 150,
         height: 150,
+    },
+    ScrollView: {
+        flex: 0.4,
+        backgroundColor: '#18181bcc',
+        padding:30
+    },
+    CurrentTempContainer: {
+        flexDirection: 'row',
+        backgroundColor: '#00000033',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        borderColor: 'white',
+        borderWidth:1,
+
+    },
+    day:{
+        fontSize: 20,
+        color:'white',
+        backgroundColor: '#3c3c44',
+        padding:10,
+        textAlign: 'center',
+        borderRadius:50,
+        fontWeight: 200,
+        marginBottom:15
     }
+
 })
 export default WeatherScroll
