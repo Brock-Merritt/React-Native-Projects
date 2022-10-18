9484c29fb399e90494eaed3e1a1885388e0e01b1
Your name here <brkmer@gmail.com>
2022-10-17 23:35:03 -0400

    if statement in futureForecast const

--- mobile-weather-app-react-native/components/FutureForecast.js
+++ mobile-weather-app-react-native/components/FutureForecast.js
@@ -4,19 +4,30 @@ import { View, Text, Image, StyleSheet  } from 'react-native'
 const FutureForecast = () => {
   return (
     <View style={{flexDirection:'row'}}>
-        <FutureForecastItem></FutureForecastItem>
-        <FutureForecastItem></FutureForecastItem>
-        <FutureForecastItem></FutureForecastItem>
+
+        {
+            data && data.length > 0 ? 
+            data.map((data, idx) => (
+                
+                idx !== 0 &&   <FutureForecastItem FutureForecastItem={data}/>
+                
+            ))
+
+            :
+            <View/>
+        }
+        
+        
 
     </View>
   )
 }
 
 const FutureForecastItem = () => {  
-    const img = {uri: 'http://openweathermap.org/img/wn/10d@2x.png'}
+    const img = {uri: 'http://openweathermap.org/img/wn/'+FutureForecastItem.weather[0].icon+'@2x.png' }
     return (
         <View style={styles.FutureForecastItemContainer}>
-            <Text style={styles.day}>Mon</Text>
+            <Text style={styles.day}>{moment(data.dt * 1000).format('dddd')}</Text>
             <Image source={img} style={styles.image} />
             <Text style={styles.temp}>Night = cold</Text>
             <Text style={styles.temp}>day = warm</Text>
--- mobile-weather-app-react-native/components/WeatherScroll.js
+++ mobile-weather-app-react-native/components/WeatherScroll.js
@@ -22,18 +22,20 @@ const CurrentTempEl= ({data}) => {
                 <Image source={img} style={styles.image}/>
                 <View style={styles.ForecastContainer}>
                     <Text style={styles.day}>{moment(data.dt * 1000).format('dddd')}</Text>
-                    <Text style={styles.temp}> Night - {data.temp.night}&#176;F</Text>
-                    <Text style={styles.temp}> Day - {data.temp.day}&#176;F</Text>
+                    <Text style={styles.temp}> Night - {data.temp.night}&#176;C</Text>
+                    <Text style={styles.temp}> Day - {data.temp.day}&#176;C</Text>
     
     
                 </View>
             </View>
-
+        )
     }else{
-        <view>
+        return(
+        <View>
 
-            
-        </view>
+
+        </View>
+        )
     }
 
     
